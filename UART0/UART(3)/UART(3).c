2eab9850f7953a91e1c3641f935e10c87260b555
SARANG SURESH <sureshsarang44@gmail.com>
2025-01-26 13:16:00 +0530

    Update UART(3).c

--- UART0/UART(3)/UART(3).c
+++ UART0/UART(3)/UART(3).c
@@ -1,1 +1,39 @@
+#include<lpc213x.h>
+void string(const char *s);
+void data(unsigned char dat); 
+char receive();  
+
+void UART0_Init(void);
+void main(){
+UART0_Init();
+data('a');
+while(1){
+
+data(receive());
+
+}
+}
+void string(const char *s){
+while(*s){
+data(*s++);
+}
+}
+void data(unsigned char dat){
+U0THR = dat;
+while(!(U0LSR & 0x20));
+}
+
+char receive(){
+while(!(U0LSR & 0x01));
+return U0RBR;
+}
+	   
+void UART0_Init(void) {
+PINSEL0 |= 0X00000005;
+U0LCR = 0x83;
+U0DLL = 97;
+U0DLM = 0x00;
+U0LCR = 0x03;
+U0TER = 0x80;
+}
 
