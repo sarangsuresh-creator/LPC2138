f1852caa725fad69a05172bb1c3fabfe7d7fb1bb
SARANG SURESH <sureshsarang44@gmail.com>
2025-04-04 11:14:15 +0530

    Update blink.c

--- blink/blink.c
+++ blink/blink.c
@@ -1,1 +1,12 @@
-
+ #include<lpc21xx.h>
+ int i=0;
+ void main(){
+ IO0DIR |= 0xffffffff;
+ 
+ while(1){
+ IO0SET |= 0xffffffff;
+ for(i=0;i<3000000;i++);
+ IO0CLR |= 0xffffffff;
+ for(i=0;i<3000000;i++);
+ }
+ }
